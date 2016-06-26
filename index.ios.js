6490c1a25dddb7d9ade74f9e3db7bc9a67fcd006
David Ernst <git@dsernst.com>
2016-06-26 15:00:41 -0700

    Fix rogue indentation in example description

--- index.ios.js
+++ index.ios.js
@@ -26,12 +26,12 @@ class harmful extends Component {
         {
           title: 'Duopoly on Political Power',
           description:
-  `Only two candidates with a chance of winning. Never effective to run an independent candidate.They're almost always very similar ideologically. Both business candidates.
+`Only two candidates with a chance of winning. Never effective to run an independent candidate.They're almost always very similar ideologically. Both business candidates.
 
-  Founding father's warnings against a two-party system: http://www.washingtonsblog.com/2011/07/the-founding-fathers-tried-to-warn-us-about-the-threat-from-a-two-party-system.html
+Founding father's warnings against a two-party system: http://www.washingtonsblog.com/2011/07/the-founding-fathers-tried-to-warn-us-about-the-threat-from-a-two-party-system.html
 
-  Helpful statements from Noam Chomsky on the subject: https://www.youtube.com/watch?v=QOudyiO238
-  `
+Helpful statements from Noam Chomsky on the subject: https://www.youtube.com/watch?v=QOudyiO238
+`
           ,
           people: ['Daniel Clough', 'Tom Bralower', 'Sean Sackett-Ferguson'],
         },
