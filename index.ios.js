617fb96a638a14ea8b99e7c98b3d9dd6090d9ed6
David Ernst <git@dsernst.com>
2016-08-30 01:42:36 -0700

    Refactor the example-data out into its own file

--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,7 @@ import {
 
 import Headerbar from './components/headerbar'
 import InactiveListItem from './components/inactive-list-item.js'
+import exampleData from './example-data.js'
 
 class harmful extends Component {
 
@@ -18,60 +19,7 @@ class harmful extends Component {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => true})
     this.state = {
       selectedIndex: '1',
-      _data: [
-        {
-          title: 'Great Pacific Garbage Patch',
-          description: '',
-          people: ['1', '2', '3'],
-        },
-        {
-          title: 'Duopoly on Political Power',
-          description:
-`Only two candidates with a chance of winning. Never effective to run an independent candidate.They're almost always very similar ideologically. Both business candidates.
-
-Founding father's warnings against a two-party system: http://www.washingtonsblog.com/2011/07/the-founding-fathers-tried-to-warn-us-about-the-threat-from-a-two-party-system.html
-
-Helpful statements from Noam Chomsky on the subject: https://www.youtube.com/watch?v=QOudyiO238
-`
-          ,
-          people: ['Daniel Clough', 'Tom Bralower', 'Sean Sackett-Ferguson'],
-        },
-        {
-          title: 'Rising Income Inequality',
-          description: '',
-          people: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42'],
-        },
-        {
-          title: 'Women\'s Leadership Gap',
-          description: '',
-          people: ['1', '2', '3', '4', '5', '6', '7', '8'],
-        },
-        {
-          title: 'Hard to Understand New Laws',
-          description: '',
-          people: [],
-        },
-        {
-          title: 'Broken Healthcare System',
-          description: '',
-          people: [],
-        },
-        {
-          title: 'Rising levels of Mass Shootings',
-          description: '',
-          people: [],
-        },
-        {
-          title: 'Anti-scientific Attitudes from Leaders',
-          description: '',
-          people: [],
-        },
-        {
-          title: 'Huge levels distrust in the political system',
-          description: '',
-          people: [],
-        },
-      ]
+      _data: exampleData,
     }
     this.state.dataSource = ds.cloneWithRows(this.state._data)
   }
